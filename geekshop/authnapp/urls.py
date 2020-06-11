93e2e856b76ad426d7a09ba480fa4c0fd1c6a26d
Denis <pda.prostor@gmail.com>
2020-06-11 05:58:25 +0300

    дз_1 " Организовать выдачу сообщения об успешной отправке письма с кодом подтверждения в окне регистрации пользователя." + верификация

--- geekshop/authnapp/urls.py
+++ geekshop/authnapp/urls.py
@@ -5,9 +5,10 @@ import authnapp.views as authnapp
 app_name = "authnapp"
 
 urlpatterns = [
+    urlpatterns = [
     re_path(r"^login/$", authnapp.login, name="login"),
     re_path(r"^logout/$", authnapp.logout, name="logout"),
     re_path(r"^register/$", authnapp.register, name="register"),
     re_path(r"^edit/$", authnapp.edit, name="edit"),
-
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$", authnapp.verify, name="verify"),
 ]
